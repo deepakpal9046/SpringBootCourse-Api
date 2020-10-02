39cc50b686563325169efd63c8186f14855e653e
Deepak Pal <deepakpal9046@users.noreply.github.com>
2020-10-02 19:15:01 +0530

    Spring Boot Starter API App

---
+++ src/main/java/com/courseapi/springbootstarter/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.courseapi.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi from new Spring web app";
+	}
+	
+
+}
---
+++ src/main/java/com/courseapi/springbootstarter/topic/TopicService.java
@@ -0,0 +1,48 @@
+package com.courseapi.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring","Spring framework","Spring framework description"),
+			new Topic("java","Core Java framework","Core Java framework description"),
+			new Topic("javascript","javascript framework","javascript framework description")
+			
+			) );
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+		
+	}
+
+}
